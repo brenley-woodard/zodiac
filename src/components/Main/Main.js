8cc314f517e6da919b44f01a3612c3bf8967d6f5
brenley-woodard <brenleyew@gmail.com>
2023-01-10 15:28:16 -0800

    added folders and files in components, rendering header, main, and footer to page

---
+++ src/components/Main/Main.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import background from '../../background.jpeg';
+import './Main.css';
+
+export default function Main() {
+  return <main style={{ backgroundImage: `url(${background})` }}>
+  </main>;
+}
